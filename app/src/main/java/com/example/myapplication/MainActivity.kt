8188182bc5efa431b288364b7a5ef8d1919bca6b
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-09-20 19:57:15 -0300

    Usage of sharedPreferences on MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,9 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val PREFERENCES_NAME = "myprefs"
+    private val KEY_NAME = "name"
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.btnSave.setOnClickListener{
+            val name = binding.etName.text.toString()
+            saveName(name)
+        }
+
+        binding.btnLoad.setOnClickListener {
+            val name = loadName()
+            binding.tvResult.text = name
+        }
+    }
+
+    private fun saveName(name: String){
+
+        val sharedPreferences = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+        //  Voy a editar mis preferences
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_NAME, name)
+        editor.apply()
+    }
+
+    private fun loadName():String? {
+        val sharedPreferences = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(KEY_NAME, "No hay nada")
     }
 }
